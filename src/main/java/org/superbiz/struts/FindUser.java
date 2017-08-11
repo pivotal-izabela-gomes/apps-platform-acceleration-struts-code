01d0e755f5ef3d8f98931471077ad12fedf416b1
Dev Floater 110 <pivotal@Dev-Floater-110.local>
2017-08-11 11:33:49 -0400

    Spring Bootification of Struts lesson

--- src/main/java/org/superbiz/struts/FindUser.java
+++ src/main/java/org/superbiz/struts/FindUser.java
@@ -17,15 +17,19 @@
  */
 package org.superbiz.struts;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import java.util.Properties;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FindUser {
 
-    private int id;
+    private long id;
     private String errorMessage;
     private User user;
+    private UserService service;
+
+    public FindUser(UserService userService) {
+        this.service = userService;
+    }
 
     public User getUser() {
         return user;
@@ -43,7 +47,7 @@ public class FindUser {
         this.errorMessage = errorMessage;
     }
 
-    public int getId() {
+    public long getId() {
         return id;
     }
 
@@ -54,12 +58,6 @@ public class FindUser {
     public String execute() {
 
         try {
-            UserService service = null;
-            Properties props = new Properties();
-            props.put(Context.INITIAL_CONTEXT_FACTORY,
-                "org.apache.openejb.core.LocalInitialContextFactory");
-            Context ctx = new InitialContext(props);
-            service = (UserService) ctx.lookup("UserServiceImplLocal");
             this.user = service.find(id);
         } catch (Exception e) {
             this.errorMessage = e.getMessage();
---
+++ src/main/java/org/superbiz/struts/Index.java
@@ -0,0 +1,9 @@
+package org.superbiz.struts;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * Created by pivotal on 8/10/17.
+ */
+public class Index extends ActionSupport {
+}
